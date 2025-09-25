8c8c5d7b6ce2ec3a7230b2c1fc2ee723a45b1c4a
unic <yuniqueunic@gmail.com>
2025-09-25 06:32:35 -0700

    feat(macos): add macOS platform support and configuration files
    - Update metadata to include macOS platform instead of Android
    - Add .gitignore specific to macOS development
    - Include Flutter debug and release configurations for macOS
    - Generate plugin registrant for macOS plugins
    - Add Podfile for managing macOS dependencies with CocoaPods

---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,24 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import ffmpeg_kit_flutter_min_gpl
+import file_selector_macos
+import geolocator_apple
+import package_info_plus
+import path_provider_foundation
+import shared_preferences_foundation
+import video_player_avfoundation
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FFmpegKitFlutterPlugin.register(with: registry.registrar(forPlugin: "FFmpegKitFlutterPlugin"))
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
+}
